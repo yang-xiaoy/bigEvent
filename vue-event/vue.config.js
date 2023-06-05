ebae68b0a0c9fc6dedacad984ce5cbd43c918ecd
杨小样随笔 <yang_xuefan@126.com>
2023-06-05 15:10:59 +0800

    vue-event v20230605

--- vue-event/vue.config.js
+++ vue-event/vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // 影响打包时，index.html引入其它资源的前缀地址
-  publicPath: './'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
 })
